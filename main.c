66983573a16b5363f8b2188b3ed5c10f8f084b82
Arsela Bita <arselabita>
2025-07-01 19:22:22 +0200

    printed only numbers if not error

--- main.c
+++ main.c
@@ -6,25 +6,47 @@ int	ft_isdigit(int c)
 		return (1);
 	return (0);
 }
+int ft_is_numeric_str(const char *str)
+{
+    int i;
+
+    if (str == NULL || *str == '\0') 
+        return (0); // Empty or null string is not numeric
+    i = 0;
+    while (str[i]) 
+    {
+        if (!ft_isdigit(str[i])) 
+            return (0); // Non-digit character found
+        i++;
+    }
+    return (1); // All characters are digits
+}
+int no_doubles()
+{
+    
+}
+
 int main(int ac, char **av)
 {
     int i;
-    int j;
-    int n;
 
-    n = ft_atoi(av[i]);
+    ;
     if (ac >= 2)
     {
         i = 1;
         while (i < ac)
         {
-            if (ft_isdigit(n))
-                printf("%d", n);
+            if (ft_is_numeric_str(av[i]) && !no_doubles(av[i], i)) // Check first if its numerical :)
+                printf("%s", av[i]);
             else
-                printf("%d its not a numeric string!!", n);
-            write(1, "\n", 1);
+            {
+                printf("its not a numeric string!!"); // Then return the message if its not numerical
+                return (0);
+            }
+            printf("\n");
             i++;
         }
     }
+    printf("\n");
     return (0);
 }

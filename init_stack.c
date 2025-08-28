6460b932f3e2cd9a66fb5080a246b71ac7985ed6
Arsela Bita <arselabita>
2025-08-28 20:34:55 +0200

    creating a stack with arrays

--- init_stack.c
+++ init_stack.c
@@ -12,39 +12,14 @@
 
 #include "push_swap.h"
 
-// ____STACK_IMPLEMENTATION_FUNCTIONS____
-// function to check if the stack is empty
-int is_empty(t_node **stack)
+t_stack create_stack(int capacity)
 {
-    return (*stack == NULL);
-}
-// function to push the elements to the stack
-void push(t_node **stack, int data)
-{
-    // here we insert data in the beginnig of the linked list stack
-    // if what is inserted is non-zero value, we have the case of 
-    // stack overflow
-    if (inserted_before_head(stack, data))
-        write(2, "Stack Overflow!\n", 17);
-}
-// function to pop an element from the stack
-int pop(t_node **stack)
-{
-    // check for underflow
-    if (is_empty(stack))
-        return (write(2, "Stack Underflow!\n", 18), -1);
-    delete_head(stack);
-}
-// function to print the stack
-void print_stack(t_node **stack)
-{
-    t_node *temp;
+    t_stack *stack;
 
-    temp = *stack;
-    while (temp != NULL)
-    {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
+    if (capacity <= 0)
+        return (write(2, "The stack is empty!\n", 21), NULL);
+    stack = (t_stack)malloc(sizeof(t_stack));
+    if (!stack)
+        return (write(2, "Failed to allocate memory for the stack!\n", 42), NULL);
+    stack.collection
 }

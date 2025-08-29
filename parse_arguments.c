8d5af12f535c9b3084de5605634db6df3c195fba
Arsela Bita <arselabita@gmail.com>
2025-08-29 13:01:07 +0200

    i split then check if it is digit

--- parse_arguments.c
+++ parse_arguments.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_arguments.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 11:42:44 by abita             #+#    #+#             */
+/*   Updated: 2025/08/29 11:42:49 by abita            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 /*
@@ -10,13 +22,36 @@
     5. check: if the input is < 3
 */
 
-
-int parse_arguments(t_stack **a, int argc, char **argv)
+int parse_arguments(t_stack *a, int argc, char **argv)
 {
     int i;
-    
-    if ()
+    int j;
+    int k;
+    int numbers;
+    char **nums;
+
+    i = 1;
+    while (i < argc)
     {
-        
+        nums = ft_split(argv[i], ' ');
+        if (!nums)
+            return (write(2, "Error: Failed to split numbers!\n", 33), 0);
+        j = 0;
+        while (nums[j])
+        {
+            k = 0;
+            if (nums[j][k] == '-' || nums[j][k] == '+')
+                k++;
+            while (nums[j][k])
+            {
+                if (!ft_isdigit(nums[j][k]))
+                    return(write(2, "ERROR: The input does not contain numbers.\n", 44), 0);
+                k++;  
+            }
+            numbers = ft_atoi(nums[j]);
+            j++;
+        }
+        i++;
     }
+    return (1);
 }

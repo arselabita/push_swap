66cb182fda5910e53cb61be72e3126bbfc33ec2b
Arsela Bita <arselabita@gmail.com>
2025-09-09 17:28:12 +0200

    created this function to handle long num

---
+++ libft/ft_atol.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 16:24:52 by abita             #+#    #+#             */
+/*   Updated: 2025/09/09 16:24:55 by abita            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <limits.h>
+
+long 	ft_atol(const char *nptr)
+{
+	long	num;
+	int		minus;
+	int		i;
+
+	minus = 1;
+	num = 0;
+	i = 0;
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			minus = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		
+		num *= 10;
+		num += nptr[i] - '0';
+		i++;
+	}
+	return (num * minus);
+}
+

1e836a5f743bc14212002e1e1a7c289052194a0e
Arsela Bita <arselabita@gmail.com>
2025-09-10 12:26:23 +0200

    removed the comments

--- src/stack.c
+++ src/stack.c
@@ -17,21 +17,14 @@ t_stack	*create_stack(int capacity)
 	t_stack	*stack;
 
 	if (capacity <= 0)
-	{
-		write(2, "The stack is empty!\n", 21);
 		return (NULL);
-	}
 	stack = (t_stack *)malloc(sizeof(t_stack));
 	if (!stack)
-	{
-		write(2, "Failed to allocate memory for the stack!\n", 42);
 		return (NULL);
-	}
 	stack->collection = malloc(sizeof(int) * capacity);
 	if (!stack->collection)
 	{
 		free(stack);
-		write(2, "Failed to create the stack!\n", 29);
 		return (NULL);
 	}
 	stack->capacity = capacity;
